31e74a4ddaf4348c6a019ad4ceb4915b724004cc
jsant <jsant@jsants-Mac-mini.local>
2026-03-13 22:28:07 -0400

    upgrade profit chart visuals

--- components/ProfitChart.js
+++ components/ProfitChart.js
@@ -1,17 +1,17 @@
-'use client'
-
 import {
   ResponsiveContainer,
   BarChart,
   Bar,
   XAxis,
   YAxis,
-  Tooltip
+  Tooltip,
+  CartesianGrid
 } from 'recharts'
 
 export default function ProfitChart({ items }) {
+
   const data = items.map((item) => ({
-    name: item.item_name || 'Item',
+    name: item.item_name || "Item",
     profit:
       Number(item.expected_sale_price || 0) -
       Number(item.purchase_price || 0) -
@@ -19,14 +19,26 @@ export default function ProfitChart({ items }) {
       Number(item.shipping_cost || 0)
   }))
 
-    return (
-    <div style={{ width: '100%', height: 320 }}>
+  return (
+    <div style={{ width: "100%", height: 320 }}>
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data}>
+
+          <CartesianGrid strokeDasharray="3 3" stroke="#1f2a44" />
+
           <XAxis dataKey="name" />
-          <YAxis />
+
+          <YAxis tickFormatter={(value) => `$${value}`} />
+
           <Tooltip />
-          <Bar dataKey="profit" fill="#f5b84d" radius={[4,4,0,0]} />
+
+          <Bar
+            dataKey="profit"
+            fill="#f5b84d"
+            radius={[4,4,0,0]}
+            barSize={60}
+          />
+
         </BarChart>
       </ResponsiveContainer>
     </div>
